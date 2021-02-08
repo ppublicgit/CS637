d4248e5f8d350d6ba0718d604fff7ccf9b409e24
p.public <p.public@protonmail.com>
2021-02-08 12:09:08 -0800

    switched test to start using iris data

--- HW1/test.py
+++ HW1/test.py
@@ -2,14 +2,56 @@ import numpy as np
 import mlp
 
 
-a = mlp.MLP(shape=(4,3,5))
 
-inputs = np.zeros((4,10))
+iris_types = {"Setosa": 0,
+              "Virginica": 2,
+              "Versicolor": 1
+              }
 
-outputs = np.zeros((5, 10))
+nn = mlp.MLP(shape=(4,3,3))
+
+#inputs = np.zeros((4,10))
+
+#outputs = np.zeros((5, 10))
+
+#breakpoint()
+
+#a.train(inputs, outputs)
+
+#a.hidden_layers
+
+
+def get_outputs(op):
+    ret = np.zeros((len(op), 3), dtype=int)
+    for i in range(len(op)):
+        idx = iris_types[op[i][-1]]
+        ret[i, idx] = 1
+    return ret
+
+
+def read_csv(rl):
+    ret = []
+    for i in range(len(rl)):
+        split = rl[i].split(",")
+        if i != len(rl)-1:
+            split[-1] = split[-1][:-1]
+        ret.append(split)
+    return ret
+
+
+def get_inputs(ret, ip):
+    for i in range(len(ip)):
+        for j in range(len(ip[0])-1):
+            ret[i, j] = ip[i][j]
+    return ret
+
+with open("iris.csv", "r") as f:
+    rl = f.readlines()[1:]
+    lines = read_csv(rl)
+    inputs = np.zeros((len(lines), len(lines[0])-1), dtype=float)
+    inp = get_inputs(inputs, lines)
+    out = get_outputs(lines)
 
 breakpoint()
 
-a.train(inputs, outputs)
-
-a.hidden_layers
+nn.train(inp, out)
