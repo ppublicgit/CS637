25c87098d0f5c660350fc1cb1b37c90234e28220
ppublic <p.public@protonmail.com>
2021-04-21 09:15:58 -0500

    full run made and perfs saved

--- HW3/rnn.py
+++ HW3/rnn.py
@@ -1,5 +1,8 @@
 import numpy as np
 from copy import deepcopy
+import os
+import pickle
+import glob
 
 np.random.seed(42)
 
@@ -11,8 +14,10 @@ class RNN():
         self.weight_init        = kwargs.get("weight_init", "rnn")
         self.num_epochs         = kwargs.get("num_epochs", 10000)
         self.progress_epoch     = kwargs.get("progress_epoch", 100)
-        self.track_epoch        = kwargs.get("track_epoch", False)
+        self.track_epoch        = kwargs.get("track_epoch", True)
+        self.save_progress      = kwargs.get("save_progress", False)
         self.seq_length         = kwargs.get("seq_length", 25)
+        self.filename_states    = kwargs.get("filename_states", None)
 
         self.alphabet           = None
         self.alphabet2idx       = {}
@@ -20,8 +25,6 @@ class RNN():
 
         self.weights            = []
         self.weights_hidden     = []
-        self.hidden_layers      = []
-        self.hidden_layers_act  = []
         self.hidden_previous    = []
         self.biases             = []
         self.memories           = []
@@ -35,6 +38,67 @@ class RNN():
                                     "rnn" : lambda x,y : np.random.randn(x, y) * 0.01,
                                     "zero" : lambda x, y : np.zeros((x+1, y), dtype=float)}
 
+        self.state_dir = (os.path.join(os.getcwd(), "states"))
+        self.progress_loaded = False
+
+        self.epoch_start = 0
+
+    def save_nn(self, epoch):
+        if self.filename_states is None:
+            print("filename_states is not set. It must be set to save nn progress.")
+            ret = input("what would you like to call your filename prefix for your saved states? ")
+            self.filename_states = ret
+
+        attributes = [self.shape, self.activation, self.learning_rate,
+                      self.num_epochs, self.progress_epoch, self.track_epoch,
+                      self.seq_length, self.alphabet, self.alphabet2idx, self.idx2alphabet]
+
+        neurons = [self.weights, self.weights_hidden, self. hidden_previous,
+                   self.biases, self.memories, self.memories_hidden,
+                   self.memories_biases]
+
+        state_info = [self.losses[-1], epoch, self.texts[-1]]
+
+        saved_info = (attributes, neurons, state_info)
+
+        out = open(os.path.join(self.state_dir, self.filename_states + f"_{epoch}.p"), "wb")
+        pickle.dump(saved_info, out)
+        out.close()
+        return
+
+
+    def load_nn(self, filename_state, epoch):
+        if not os.path.exists(os.path.join(self.state_dir, filename_state + f"_{epoch}.p")):
+            raise ValueError(f"Filename {filename_state} does not exist")
+        infile = open(os.path.join(self.state_dir, filename_state + f"_{epoch}.p"), "rb")
+        loaded_info = pickle.load(infile)
+
+        self.filename_states = filename_state
+        self.progress_loaded = True
+
+        attributes, neurons, state_info = loaded_info
+
+        self.shape, self.activation, self.learning_rate, \
+        self.num_epochs, self.progress_epoch, self.track_epoch, \
+        self.seq_length, self.alphabet, self.alphabet2idx, self.idx2alphabet = attributes
+
+        self.weights, self.weights_hidden, self. hidden_previous, \
+        self.biases, self.memories, self.memories_hidden, \
+        self.memories_biases = neurons
+
+        self.losses = [state_info[0]]
+        self.back_losses = [state_info[0]]
+        self.texts = [state_info[2]]
+        self.epoch_start = state_info[1]+1
+
+        infile.close()
+
+        print("=" * 10)
+        print(f"Loading rnn state from file {filename_state}")
+        print(f"Last error : {self.losses[-1]} \t Last epoch : {self.epoch_start-1}")
+        print(f"Last text:\n{self.texts[-1]}")
+        print("=" * 10)
+        return
 
     def _setup_architecture(self):
         """Setup the neural network's architcecture
@@ -48,8 +112,6 @@ class RNN():
         for i in range(len(self.shape)-2):
             self.weights.append(self.init_weight_funcs[self.weight_init](self.shape[i+1], self.shape[i]))
             self.weights_hidden.append(self.init_weight_funcs[self.weight_init](self.shape[i+1], self.shape[i+1]))
-            self.hidden_layers.append(None)
-            self.hidden_layers_act.append(None)
             self.biases.append(np.zeros((self.shape[i+1], 1)))
             self.hidden_previous.append(np.zeros((self.shape[i+1], 1)))
 
@@ -65,35 +127,51 @@ class RNN():
 
 
     def train(self, data):
-        self.alphabet = sorted(list(set(data)))
-        for idx, char in enumerate(self.alphabet):
-            self.alphabet2idx[char] = idx
-            self.idx2alphabet[idx] = char
+        if not self.progress_loaded:
+            self.alphabet = sorted(list(set(data)))
+            for idx, char in enumerate(self.alphabet):
+                self.alphabet2idx[char] = idx
+                self.idx2alphabet[idx] = char
 
-        self._setup_architecture()
+            self._setup_architecture()
 
-        self.losses = [-np.log(1.0/len(self.alphabet)) * self.seq_length] # loss at time 0
+            self.back_losses = [-np.log(1.0/len(self.alphabet)) * self.seq_length] # loss at time 0
+            self.losses = []
+            self.texts = []
+        else:
+            alphabet = sorted(list(set(data)))
+            if alphabet != self.alphabet:
+                raise ValueError("Input file alphabet does not match rnn's loaded alphabet")
 
         seq = 0
         for i in range(self.num_epochs):
             if seq + self.seq_length + 1 >= len(data): # reset the sequencing of data
                 seq = 0
                 for i in range(len(self.shape)-2):
-                    self.hidden_previous[i] = np.zeros((self.shape(i+1), 1))
+                    self.hidden_previous[i] = np.zeros((self.shape[i+1], 1))
 
             inputs = [self.alphabet2idx[char] for char in data[seq:seq+self.seq_length]]
             targets = [self.alphabet2idx[char] for char in data[seq+1:seq+self.seq_length+1]]
 
 
             back_loss = self._backward(inputs, targets)
-            loss = self.losses[-1] * 0.999 + back_loss * 0.001
-            self.losses.append(loss)
-
+            loss = self.back_losses[-1] * 0.999 + back_loss * 0.001
+            self.back_losses.append(loss)
             seq += self.seq_length
-            if i % self.progress_epoch == 0:
-                print(f"iter {i}, loss: {loss}")
+            if (i + self.epoch_start) % self.progress_epoch == 0:
+                self.losses.append(loss)
+                print("")
+                print(f"iter {self.epoch_start + i}, loss: {loss}")
                 gen = self.generate(self.idx2alphabet[inputs[0]], 200)
                 print('----\n %s \n----' % (gen, ))
+                if self.track_epoch or self.save_progress:
+                    self.texts.append(gen)
+                if self.save_progress:
+                    self.save_nn(i+self.epoch_start)
+            elif (i + self.epoch_start) % int(self.progress_epoch/10) == 0:
+                print(".", end="", flush=True)
+
+
 
 
     def _backward(self, inputs, targets):
@@ -227,8 +305,52 @@ class RNN():
         return ''.join(chars)
 
 
+def load_state_perfs(filename):
+    import glob
+    files = glob.glob(os.path.join(os.getcwd(), "states", f"{filename}*.p"))
+    losses, texts, epochs = [], [], []
+    for f in files:
+        infile = open(f, "rb")
+        loaded_info = pickle.load(infile)
+
+        _, _, state_info = loaded_info
+
+        losses.append(state_info[0])
+        texts.append(state_info[2])
+        epochs.append(state_info[1])
+
+        infile.close()
+
+    return losses, texts, epochs
+
+
+def save_state_perfs_to_pickle(filename, losses, texts, epochs):
+    outfile = open(filename, "wb")
+    save = (losses, texts, epochs)
+    pickle.dump(save, outfile)
+    outfile.close()
+
+
+def load_perfs_only_file(filename):
+    infile = open(filename, "rb")
+    load = pickle.load(infile)
+
+    losses, texts, epochs = load[0], load[1], load[2]
+
+    infile.close()
+
+    return losses, texts, epochs
+
+
 if __name__ == "__main__":
     data = open('input.txt', 'r').read() # should be simple plain text file
 
-    rnn = RNN((512, 512, 512))
+    rnn = RNN((128,128,128,),
+              num_epochs=1000000,
+              progress_epoch=1000,
+              track_epoch=True,
+              filename_states="",
+              seq_length=100,
+              save_progress=False)
+    #rnn.load_nn("rnn_shakespeare_long", 0)
     rnn.train(data)
